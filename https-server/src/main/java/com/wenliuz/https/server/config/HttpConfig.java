eb57091bd551a992e1fa5ef1bd1c5deadb173c6b
wenliuz <wenliuz@qq.com>
2019-01-02 14:19:47 +0800

    add https

---
+++ https-server/src/main/java/com/wenliuz/https/server/config/HttpConfig.java
@@ -0,0 +1,57 @@
+package com.wenliuz.https.server.config;
+
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.File;
+import java.io.IOException;
+
+@Configuration
+public class HttpConfig {
+
+    /* --------------------请按照自己spring boot版本选择 start--------------------- */
+    // 这是spring boot 1.5.X以下版本的 添加了这个，下一个就不用添加了
+    /*@Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector()); // 添加http
+        return tomcat;
+    }*/
+
+    // 这是spring boot 2.0.X版本的 添加这个，上一个就不用添加了
+    /*@Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector()); // 添加http
+        return tomcat;
+    }*/
+
+    /* --------------------请按照自己spring boot版本选择 end--------------------- */
+
+    // 配置http
+    private Connector createStandardConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(8080);
+        return connector;
+    }
+
+    @Bean
+    public Connector httpConnector(){
+        Connector connector=new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(8081);
+        return connector;
+    }
+
+}
